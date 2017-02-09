712dc15cbaac2b6b3fd44f815974a9162ebaf075
valeriyleman <vle@sitecore.net>
2017-02-09 12:31:39 +0200

    initial fix

---
+++ src/Sitecore.Support.131992/ApplyFieldMappingRule.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sitecore.Support.ContentSearch.SolrProvider.Pipelines.FormatQueryFieldValue
+{
+  using Sitecore.ContentSearch.Pipelines.FormatQueryFieldValue;
+  using Sitecore.ContentSearch.SolrProvider.Pipelines.FormatQueryFieldValue;
+  using System;
+  using System.Collections.Generic;
+  using System.Collections.ObjectModel;
+
+  public class ApplyFieldMappingRule : Sitecore.ContentSearch.SolrProvider.Pipelines.FormatQueryFieldValue.ApplyFieldMappingRule
+  {
+    protected override void FormatArgs(string schemaType, FormatQueryFieldValueArgs args)
+    {
+      if (!string.IsNullOrEmpty(schemaType))
+      {
+        bool flag = !string.IsNullOrEmpty(args.EscapeCharacter) && args.IncludeExistingCharacter.HasValue;
+        switch (schemaType.ToLower())
+        {
+          case "text":
+          case "text_general":
+            {
+              string str2 = " ";
+              Collection<string> additionalEscapeCharacters = new Collection<string> { "-" };
+              args.FieldValue = this.Escape(args.FieldValue, !string.IsNullOrEmpty(args.EscapeCharacter) ? args.EscapeCharacter : str2, flag ? args.IncludeExistingCharacter.Value : false, args.ExcludeEscapeCharacters, additionalEscapeCharacters);
+              break;
+            }
+          case "string":
+          case "lowercase":
+            args.FieldValue = this.Escape(args.FieldValue, !string.IsNullOrEmpty(args.EscapeCharacter) ? args.EscapeCharacter : @"\", flag ? args.IncludeExistingCharacter.Value : true, args.ExcludeEscapeCharacters, null);
+            args.IsQuoted = false;
+            break;
+        }
+        args.FieldValue = args.FieldValue.Trim();
+      }
+    }
+
+    protected override HashSet<string> GetEscapeCharacterSet() =>
+        new HashSet<string> {
+              "+",
+              "&",
+              "|",
+              "!",
+              "{",
+              "}",
+              "[",
+              "]",
+              "^",
+              "(",
+              ")",
+              "~",
+              ":",
+              ";",
+              "/",
+              @"\",
+              "?",
+              "\"",
+              " "
+        };
+  }
+}
